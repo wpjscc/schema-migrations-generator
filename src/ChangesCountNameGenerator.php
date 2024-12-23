b0bab72789594675c151c0e3a30e75ca8ad1f73a
roxblnfk <roxblnfk@ya.ru>
2024-12-23 15:24:31 +0400

    Comment `ChangesCountNameGenerator`

--- src/ChangesCountNameGenerator.php
+++ src/ChangesCountNameGenerator.php
@@ -8,6 +8,19 @@ use Cycle\Migrations\Atomizer\Atomizer;
 use Cycle\Schema\Generator\Migrations\Changes\ChangeType;
 use Cycle\Schema\Generator\Migrations\Changes\Collector;
 
+/**
+ * Generates migration name based on changes count.
+ *
+ * Like:
+ *      ct1_dt1_t2_c3_i3_fk3
+ * There are:
+ * - 1 create table
+ * - 1 drop table
+ * - 2 table changes
+ * - 3 column changes
+ * - 3 index changes
+ * - 3 foreign key changes
+ */
 final class ChangesCountNameGenerator implements NameGeneratorInterface
 {
     public function generate(Atomizer $atomizer): string
